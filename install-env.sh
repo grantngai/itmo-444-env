1cbc8426f62152c494358f08af6d9a5294568629
grantngai <pngai@hawk.iit.edu>
2015-12-11 23:16:39 -0600

    added install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone http://github.com/grantngai/itmo-444-fall2015.git
+git clone https://github.com/grantngai/itmo-444-fall2015.git
 
 mv ./itmo-444-fall2015/images /var/www/html/images
 mv ./itmo-444-fall2015/*.html /var/www/html
